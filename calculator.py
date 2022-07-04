37cd6a7e26af5879d82cc98d712c3baa1a999198
Mansour Alh <mansouralhussain94@gmail.com>
2022-07-04 20:09:36 +0300

    done

--- calculator.py
+++ calculator.py
@@ -1,8 +1,51 @@
 
+def calculator ():
+
+	number1= input("Enter the first number:")
+	
+	number2= input("Enter the second number:")
+	if number2.isdigit() and number1.isdigit():
+		 
+		
+	
+
+		operation1= input("Choose the operation (+, -, /, *):")
+		if operation1 == "+":
+			total = (int(number1)+int(number2))
+			print (f"the answer is {total}")
+		elif operation1 =="*":
+			total = (int(number1)*int(number2))
+			print (f"the answer is {total}")
+		elif operation1 =="-":
+			total = (int(number1)-int(number2))
+			print (f"the answer is {total}")
+		elif operation1 =="/":
+				total = (int(number1))/(int(number2))
+				
+		else:
+				print ("the operation is not valid")
+
+
+			
+	else:
+		print ("the numbers were invalid")
+
+
+    
+
+
 def main():
 	#write your code here
 	pass
-	
+
+
+
+
+
+
+
+calculator()
+
 
 
 
